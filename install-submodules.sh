dd79a8a36d56a19068e620996aedccf3f3f75007
Danya D. Smith <danyadsmith@gmail.com>
2018-07-29 19:37:14 -0400

    Create a Script to Install Submodules

---
+++ install-submodules.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# INSTALL DEFAULT PLUGINS and COLORSCHEMES FOR VIM/nVIM
+
+SCRIPT_NAME="get-vim-plugins.sh"
+SCRIPT_AUTHOR="Danya D. Smith"
+SCRIPT_UPDATED="Saturday 28 July 2018 @ 6:17 PM"
+
+echo "Executing: $SCRIPT_NAME"
+echo "By: $SCRIPT_AUTHOR"
+echo "Last Updated: $SCRIPT_UPDATED"
+echo "========================================================================="
+echo ""
+
+echo "Installing Submodule: base16-vim"
+git submodule add https://github.com/chriskempson/base16-vim vim/iOS/.vim/bundle/base-16-vim
+
+echo "Installing Submodule: lightline.vim"
+git submodule add https://github.com/itchyny/lightline.vim vim/iOS/.vim/bundle/lightline.vim
+
+echo "Installing Submodule: vim-pathogen"
+git submodule add https://github.com/tpope/vim-pathogen.git vim/iOS/.vim/bundle/vim-pathogen
+
+echo "Installing Submodule: ctrlp"
+git submodule add https://github.com/ctrlpvim/ctrlp.vim.git vim/iOS/.vim/bundle/ctrlp.vim
+
+echo "Installing Submodule: tabular"
+git submodule add https://github.com/godlygeek/tabular.git vim/iOS/.vim/bundle/tabular
+
+echo "Installing Submodule: vim-coloresque"
+git submodule add https://github.com/Marslo/vim-coloresque.git vim/iOS/.vim/bundle/vim-coloresque
+
+echo "Installing Submodule: vim-markdown-enhancements"
+git submodule add https://github.com/mattly/vim-markdown-enhancements.git vim/iOS/.vim/bundle/vim-markdown-enhancements
+
+echo "Installing Submodule: vim-repeat"
+git submodule add https://github.com/tpope/vim-repeat.git vim/iOS/.vim/bundle/vim-repeat
+
+echo "Installing Submodule: vim-surround"
+git submodule add https://github.com/tpope/vim-surround.git vim/iOS/.vim/bundle/vim-surround
+
+echo "Installing Submodule: emmet-vim"
+git submodule add https://github.com/mattn/emmet-vim.git vim/iOS/.vim/bundle/emmet-vim
+
+echo "Installing Submodule: vimwiki"
+git submodule add https://github.com/vimwiki/vimwiki.git vim/iOS/.vim/bundle/vimwiki
+
+echo "Installing Submodule: calendar.vim"
+git submodule add https://github.com/itchyny/calendar.vim.git vim/iOS/.vim/bundle/calendar.vim
+
+echo "Installing Submodule: tlib_vim"
+git submodule add https://github.com/tomtom/tlib_vim.git vim/iOS/.vim/bundle/tlib_vim
+
+echo "Installing Submodule: vim-addon-mw-utils"
+git submodule add https://github.com/MarcWeber/vim-addon-mw-utils.git vim/iOS/.vim/bundle/vim-addon-mw-utils
+
+echo "Installing Submodule: vim-snipmate"
+git submodule add https://github.com/garbas/vim-snipmate.git vim/iOS/.vim/bundle/vim-snipmate
+
+echo "Installing Submodule: my-vim-snippets"
+git submodule add https://github.com/danyadsmith/my-vim-snippets.git vim/iOS/.vim/bundle/my-vim-snippets
