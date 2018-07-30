afab0e359c2fd0ab97ed05c5ef0e1d18e08707b7
Danya D. Smith <danyadsmith@gmail.com>
2018-07-29 21:19:13 -0400

    Update Script to Install Default Vim Plugins
    Add my personal repository of Vim snippets.

--- vim/Main/get-vim-plugins.sh
+++ vim/Main/get-vim-plugins.sh
@@ -43,6 +43,11 @@ git clone https://github.com/itchyny/lightline.vim
 echo "========================================================================="
 echo ""
 
+echo "Installing Plugin: my-vim-snippets"
+git clone https://github.com/danyadsmith/my-vim-snippets
+echo "========================================================================="
+echo ""
+
 echo "Installing Plugin: tabular"
 git clone https://github.com/godlygeek/tabular
 echo "========================================================================="
@@ -52,7 +57,6 @@ echo "Installing Plugin: SnipMate"
 git clone https://github.com/tomtom/tlib_vim.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
 git clone https://github.com/garbas/vim-snipmate.git
-git clone https://github.com/honza/vim-snippets.git
 echo "========================================================================="
 echo ""
 
